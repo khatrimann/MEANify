9f69f2b3b19c547c2ecc8549bb6e7f277ceae04a
Mann Khatri <khatri.mann@gmail.com>
2019-03-30 16:05:41 +0530

    Retrieve

--- upload-app/upload/routes/index.js
+++ upload-app/upload/routes/index.js
@@ -9,16 +9,19 @@ var fs = require('fs');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   var gfs = Grid(mongoose.connection.db);
-  var readstream = gfs.createReadStream({_id: '5c9f2fcc545e2f364172067a'});
-  var buffer = "";
-
-  readstream.on("data", function (chunk) {
-    buffer += chunk;
-  });
-
-  readstream.on("end", function () {
-    console.log("contents of file:\n\n", buffer);
-  });
+  var data_db = [];
+  mongoose.connection.db.collection("fs.files", function(err, collection){
+    collection.find({}).toArray(function(err, data){
+        console.log(data); // it will print your collection data
+        for(var i=0;i<data.length;i++) {
+          console.log(data[i]._id);
+          var readstream = gfs.createReadStream({_id: data[i]._id});
+          readstream.pipe(res);
+        }
+    });
+});
+  // var readstream = gfs.createReadStream({_id: '5c9f2fcc545e2f364172067a'});
+  // readstream.pipe(res);
 });
 
 module.exports = router;
