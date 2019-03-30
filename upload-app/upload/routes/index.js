00ac53f1afeb339f935eb2fe468a200b85c2a03b
Mann Khatri <khatri.mann@gmail.com>
2019-03-30 14:58:09 +0530

    Fetching from GridFS

--- upload-app/upload/routes/index.js
+++ upload-app/upload/routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require("mongoose");
+const Grid = require('gridfs-stream');
+Grid.mongo = mongoose.mongo;
+var fs = require('fs');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var gfs = Grid(mongoose.connection.db);
+  var readstream = gfs.createReadStream({_id: '5c9f2fcc545e2f364172067a'});
+  var buffer = "";
+
+  readstream.on("data", function (chunk) {
+    buffer += chunk;
+  });
+
+  readstream.on("end", function () {
+    console.log("contents of file:\n\n", buffer);
+  });
 });
 
 module.exports = router;
